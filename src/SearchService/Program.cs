e8fad63d105407027206a5858a883816aa226a22
Mario <canalessanchezmario@gmail.com>
2024-02-12 17:45:38 +0100

    Cambios para la comunicacion http entre micros. Se mejora la resiliencia del servicio si no encuntra o se realiza la peticion http

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,9 @@
 
+using System.Net;
+using Microsoft.VisualBasic;
+using MongoDB.Bson.Serialization.Conventions;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService;
 using SearchService.Data;
 
@@ -7,8 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,13 +23,24 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception e)
-{
-    Console.WriteLine(e);
-}
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
+
+
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
