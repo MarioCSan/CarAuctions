817c5323931bfb87fb5de511443f929061ae2a73
Mario <canalessanchezmario@gmail.com>
2024-02-12 12:34:30 +0100

    Se crea la comunicacion HTTP entre servicios y se comprueba quye search service puede comunicarse con AuctionServices

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,5 @@
 
+using SearchService;
 using SearchService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 var app = builder.Build();
 
